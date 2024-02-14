2926596027d9e6b99608888d70f2f5f9abf8a472
Topi Karilainen <topi.karilainen@moonglow.fi>
2024-02-14 16:09:23 +0200

    Add modular structure to JavaScript

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,20 +1,3 @@
-async function getIssues() {
-    try {
-        const response = await fetch("/issues?projectAbbreviation=SIA");
-        if (!response.ok) {
-            console.error("Response not OK: " + response.status);
-            return;
-        }
-        const issues = await response.json();
-        const contentElem = document.getElementById("issues");
-        for (let issue of issues) {
-            const issueElem = document.createElement("div");
-            issueElem.textContent = `${issue.project.abbreviation}-${issue.id} ${issue.type} ${issue.title} ${issue.creator.fullName} ${issue.assignee.fullName} ${issue.status} ${issue.priority} ${issue.description}`
-            contentElem.appendChild(issueElem);
-        }
-    } catch (error) {
-        console.error("Error fetching issues:", error);
-    }
-}
+import { getIssues } from "./modules/issue-fetcher.js";
 
 getIssues();
